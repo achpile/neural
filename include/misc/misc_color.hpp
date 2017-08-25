51e837be7111dca5e4291534b0a454149c0cdf4b
Fedosov Alexander <achpile@gmail.com>
2017-08-25 23:37:44 +0300

    Color class

---
+++ include/misc/misc_color.hpp
@@ -0,0 +1,29 @@
+/***********************************************************************
+     * File       : misc_color.hpp
+     * Created    : Aug 25, 2017
+     * Copyright  : (C) 2017 Achpile
+     * Author     : Fedosov Alexander
+     * Email      : achpile@gmail.com
+
+***********************************************************************/
+#ifndef __MISC_COLOR
+#define __MISC_COLOR
+
+
+namespace ach {
+	struct Color {
+		float r;
+		float g;
+		float b;
+
+
+		Color();
+
+		void bound();
+
+		ach::Color operator+(const ach::Color t);
+		ach::Color operator*(const float t);
+	};
+}
+
+#endif
