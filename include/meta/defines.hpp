de4c11832e54a5ac48d0f88beccb549f4918e090
Fedosov Alexander <achpile@gmail.com>
2017-11-22 16:16:01 +0300

    10 birds

--- include/meta/defines.hpp
+++ include/meta/defines.hpp
@@ -34,7 +34,7 @@
 #define FLAPPY_SPEED   200.0f
 #define FLAPPY_DIFF    300.0f
 #define FLAPPY_HIGH     75.0f
-#define FLAPPY_COUNT   500
+#define FLAPPY_COUNT   10
 #define FLAPPY_LIMIT_T 125.0f
 #define FLAPPY_LIMIT_B 415.0f
 
